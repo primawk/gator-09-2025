d5e7850e15163ef713c2b3c7ef20e363f915dbde
primawk <pwkuntoro@gmail.com>
2025-10-25 14:45:29 +0700

    add feed table schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -9,3 +9,15 @@ export const users = pgTable("users", {
     .$onUpdate(() => new Date()),
   name: text("name").notNull().unique(),
 });
+
+export const feeds = pgTable("feeds", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  name: text("name").notNull(),
+  url: text("url").notNull().unique(),
+  userId: text("user_id").notNull(),
+});
