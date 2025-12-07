029050c857137c67940805e3364856d25bb272cf
primawk <pwkuntoro@gmail.com>
2025-12-07 10:18:52 +0700

    add getPostsForUser function

--- src/lib/db/queries/posts.ts
+++ src/lib/db/queries/posts.ts
@@ -1,5 +1,6 @@
 import { db } from "..";
 import { posts } from "../schema";
+import { desc, eq } from "drizzle-orm";
 
 export async function createPost(title: string, url: string, feedId: string) {
   const [result] = await db
@@ -7,4 +8,14 @@ export async function createPost(title: string, url: string, feedId: string) {
     .values({ title: title, url: url, feedId: feedId })
     .returning();
   return result;
+}
+
+export async function getPostsForUser(feedId: string, limit = 2) {
+  const results = await db
+    .select()
+    .from(posts)
+    .where(eq(posts.feedId, feedId))
+    .orderBy(desc(posts.publishedAt))
+    .limit(limit);
+  return results;
 }
